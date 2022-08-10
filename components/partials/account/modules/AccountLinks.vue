87b10c568ab6bef9153f0c9c46e0f3cb66c84e6d
Enrique Sarmiento <jeisonsarmiento@correo.unimet.edu.ve>
2022-08-10 13:42:22 -0400

    bug wishlist solved

--- components/partials/account/modules/AccountLinks.vue
+++ components/partials/account/modules/AccountLinks.vue
@@ -1,46 +1,55 @@
 <template lang="html">
-    <ul>
-        <template v-for="link in links">
-            <li :key="link.text" :class="link.active ? 'active' : '' " @click="idk(link.text)" >
-                <nuxt-link :to="link.url">
-                    <i :class="link.icon"></i>
-                    {{ link.text }}
-                </nuxt-link>
-            </li>
-        </template>
-        <li>
-            <a href="#" @click.prevent="handleLogout">
-                <i class="icon-power-switch"></i>
-                Cerrar Sesión
-            </a>
-            
-        </li>
-    </ul>
+	<ul>
+		<template v-for="link in links">
+			<li
+				:key="link.text"
+				:class="link.active ? 'active' : ''"
+				@click="idk(link.text)"
+			>
+				<nuxt-link :to="link.url">
+					<i :class="link.icon"></i>
+					{{ link.text }}
+				</nuxt-link>
+			</li>
+		</template>
+		<li v-if="user">
+			<a href="#" @click.prevent="handleLogout">
+				<i class="icon-power-switch"></i>
+				Cerrar Sesión
+			</a>
+		</li>
+		<li v-else>
+			<nuxt-link to="/account/login">
+				<i class="icon-power-switch"></i>
+				Iniciar sesion
+			</nuxt-link>
+		</li>
+	</ul>
 </template>
 
 <script>
-import {mapGetters} from 'vuex';
+import { mapGetters } from "vuex";
 export default {
-    name: 'AccountLinks',
-    props: [
-        'breadinfo'
-    ],
-    computed: {
-        ...mapGetters({
-            links: 'app/getLinks'
-        })
-    },
-    methods: {
-        idk(name){
-            const nameLink = name;
-            this.breadinfo(nameLink)
-        },
-        handleLogout() {
-            this.$store.dispatch('auth/setAuthStatus', false);
-            this.$router.push('/');
-        }
-    }
-
+	name: "AccountLinks",
+	props: ["breadinfo"],
+	computed: {
+		...mapGetters({
+			links: "app/getLinks",
+		}),
+		user() {
+			return this.$cookies.get("auth");
+		},
+	},
+	methods: {
+		idk(name) {
+			const nameLink = name;
+			this.breadinfo(nameLink);
+		},
+		handleLogout() {
+			this.$store.dispatch("auth/setAuthStatus", false);
+			this.$router.push("/");
+		},
+	},
 };
 </script>
 
--- components/partials/account/modules/AsideAccount.vue
+++ components/partials/account/modules/AsideAccount.vue
@@ -1,33 +1,33 @@
 <template>
-  <aside class="ps-widget--account-dashboard">
-    <div class="ps-widget__header">
-      <div>
-        <h3>¡Bienvendo {{ user.user.username }}!</h3>
-      </div>
-    </div>
-    <div class="ps-widget__content">
-      <AccountLinks :breadinfo="getLinkName" />
-    </div>
-  </aside>
+	<aside class="ps-widget--account-dashboard">
+		<div class="ps-widget__header">
+			<div v-if="user">
+				<h3>¡Bienvendo {{ user.user.username }}!</h3>
+			</div>
+		</div>
+		<div class="ps-widget__content">
+			<AccountLinks :breadinfo="getLinkName" />
+		</div>
+	</aside>
 </template>
 
 <script>
 import AccountLinks from "./AccountLinks";
 
 export default {
-  name: "AsideAccount",
-  props: ["bread"],
-  components: { AccountLinks },
-  computed: {
-    user() {
-      return this.$cookies.get("auth");
-    },
-  },
-  methods: {
-    getLinkName(datos) {
-      this.bread(datos);
-    },
-  },
+	name: "AsideAccount",
+	props: ["bread"],
+	components: { AccountLinks },
+	computed: {
+		user() {
+			return this.$cookies.get("auth");
+		},
+	},
+	methods: {
+		getLinkName(datos) {
+			this.bread(datos);
+		},
+	},
 };
 </script>
 
