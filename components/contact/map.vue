6484f8dffb64a37cbffc498ae5ed2f660d1cfa83
David Arenas <davejs136@gmail.com>
2023-05-24 12:36:47 -0400

    add: contact page and thanks page

---
+++ components/contact/map.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="w-full">
+    <iframe
+      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3923.2234255568255!2d-66.883192!3d10.483045500000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8c2a596fce41c7cd%3A0x7ce2023bd01698b!2sPonchola%20Parkas%20Deportivas!5e0!3m2!1ses-419!2sus!4v1663860936249!5m2!1ses-419!2sus"
+      class="w-full"
+      height="500"
+      style="border: 0"
+      allowfullscreen
+      loading="lazy"
+      referrerpolicy="no-referrer-when-downgrade"
+    ></iframe>
+  </div>
+</template>
